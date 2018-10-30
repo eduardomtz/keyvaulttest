36567617cd081c8d2f2842ddd6b6114caeb0501e
Eduardo David Martinez Neri <edmarti@microsoft.com>
2018-10-30 16:28:46 -0700

    Add project files.

---
+++ WebEdKeyVault/Pages/About.cshtml.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Configuration;
+
+namespace WebEdKeyVault.Pages
+{
+    public class AboutModel : PageModel
+    {
+        public AboutModel(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        private readonly IConfiguration _configuration = null;
+
+        public string Message { get; set; }
+
+        public void OnGet()
+        {
+            Message = "My key val = " + _configuration["AppSecret"];
+        }
+    }
+}
